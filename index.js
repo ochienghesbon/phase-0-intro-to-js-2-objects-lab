4a407321af21861e0f1b6883b3bc78a7f5cd9c3b
ochienghesbon <ochienghesbon@student.moringaschool.com>
2022-06-02 10:18:57 +0300

    commit tree dfae8528ded414884e5f82a000b646d29f4d9339

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Write your solution in this file!
+const employee = {
+    name: "ebbe",
+    streetAddress: "leo",
+  };
+  function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const newemployee = { ...employee };
+    newemployee.name = "Sam";
+    newemployee.streetAddress = "11 Broadway";
+  
+    return newemployee;
+  }
+  function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee.name = "Sam";
+    employee.streetAddress = "12 Broadway";
+  
+    return employee;
+  }
+  function deleteFromEmployeeByKey(employee, key) {
+    const newemployee = { ...employee };
+    delete newemployee.name;
+    delete newemployee.streetAddress;
+  
+    return newemployee;
+  }
+  function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee.name;
+    delete employee.streetAddress;
+  
+    return employee;
+  }
